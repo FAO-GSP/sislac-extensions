55b4c49bca7f78a2268113f9c0d7d11d60c37b8e
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-08-20 13:40:26 -0300

    Explicit include is not needed

--- lib/sislac/engine.rb
+++ lib/sislac/engine.rb
@@ -1,12 +1,5 @@
 module Sislac
   class Engine < ::Rails::Engine
-    config.before_initialize do
-      ActiveSupport.on_load :action_controller do
-        # Include helpers on main app
-        include Sislac::ApplicationHelper
-      end
-    end
-
     # Make every asset visible on main app
     config.assets.precompile += %w(
       sislac/app.svg
