7f8759a0cdbbfa7c40d520109c76b4a4ecf1d78e
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-09-14 03:48:53 -0300

    Provide a list of valid country codes

--- lib/sislac.rb
+++ lib/sislac.rb
@@ -1,5 +1,12 @@
-require "sislac/engine"
+require 'sislac/engine'
 
 module Sislac
-  # Your code goes here...
+  # List of ISO 3166-1 alpha-3 country codes accepted by the application
+  mattr_accessor :default_country_codes
+  @@default_country_codes = %w{
+    ABW AIA ARG ATG BES BHS BLM BLZ BMU BOL BRA BRB CHL COL CRI CUB CUW CYM DMA
+    DOM ECU FLK GLP GRD GTM GUF GUY HND HTI JAM KNA LCA MAF MEX MSR MTQ NIC PAN
+    PER PRI PRY SGS SLV SUR SXM TCA TTO UMI URY VCT VEN VGB VIR XCL
+    ANT
+  }
 end
--- lib/sislac/engine.rb
+++ lib/sislac/engine.rb
@@ -1,5 +1,9 @@
 module Sislac
   class Engine < ::Rails::Engine
+    # The hosting application calls Rails.configuration.engine to access this
+    # engine's methods
+    config.engine = Sislac
+
     # Make every asset visible on main app
     config.assets.precompile += %w(
       sislac/app.svg
